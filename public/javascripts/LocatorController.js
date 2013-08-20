50bb53168557213ab3ae935f192f30678b213d17
Radu Stoenescu <radu_stoenescu@yahoo.com>
2013-08-21 00:03:22 +0300

    Remove validations.

--- public/javascripts/LocatorController.js
+++ public/javascripts/LocatorController.js
@@ -20,27 +20,8 @@ function LocatorController($scope, $http) {
 	  }
 	};
 
-/*	Input validation functions
-*/	
-	$scope.valid_lat_lon = function() {
-		return /^-?\d+(\.\d+)?,\s*-?\d+(\.\d+)?$/.test($scope.coordinates);
-	}
-
-	$scope.valid_ip = function() {
-		return /^\d{1,3}(\.\d{1,3}){3}$/.test($scope.ip);
-	}
-
-	$scope.get_location_name = function(){
-		alert($scope.valid_lat_lon());
-		alert($scope.valid_ip());
-	}
-
 	$scope.get_location_name = function() {
-		if (! ($scope.valid_lat_lon() || $scope.valid_ip())) {
-			alert("Neither the coordinates nor the IP address seems right.\nGive it another shot.");
-		}
-
-    	var postData = {coordinates: $scope.coordinates, ip: $scope.ip};
+		var postData = {coordinates: $scope.coordinates, ip: $scope.ip};
     	
     	$http.post('query', postData).success( function(data, status, headers, config) {
     		console.log(status + data);
