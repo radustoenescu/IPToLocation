388a72992db52aecf68616befd0d86e817a38413
Radu Stoenescu <radu_stoenescu@yahoo.com>
2013-08-22 00:29:18 +0300

    Make fallback chain.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -27,7 +27,7 @@ object Application extends Controller {
       val latitude = coordinates(0).trim
       val longitude = coordinates(1).trim
 
-      val request: Future[String] = buildGoogleRequest(latitude, longitude)
+      val request: Future[String] = serviceArrgregator(latitude, longitude)
 
       Async {
         request.map { response =>
@@ -78,4 +78,30 @@ object Application extends Controller {
     }
   }
 
+  private def buildBingRequest(latitude: String, longitude: String): Future[String] = {
+    val url = "http://dev.virtualearth.net/REST/v1/Locations/" + latitude +
+    "," + longitude + "?o=json&key=AgC3EDAI9VgAoukryRvih86t9M9WUEOhkC7xu7VV0AF6W5aVNPvz6thRnoeRMTRg"
+
+    WS.url(url).get().
+      map { response => response.json
+    }.filter { response => (response \ "statusDescription").asOpt[String] match {
+      //            make sure status is ok
+      case Some(value) => value equals "OK"
+      case None => false
+    }
+    }.map { response =>
+      (response \ "resourceSets").asOpt[List[JsObject]] match {
+        case Some(results) if results.length > 0 =>
+          Logger info (results.mkString("\n"))
+
+          ((results(0) \ "resources")(0) \ "address" \ "locality").toString
+
+        case None => throw new NoSuchElementException
+      }
+    }
+  }
+
+  private def serviceArrgregator(latitude: String, longitude: String): Future[String] = {
+    buildGoogleRequest(latitude, longitude) fallbackTo buildBingRequest(latitude, longitude)
+  }
 }
